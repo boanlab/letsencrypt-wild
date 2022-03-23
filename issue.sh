48f86f522c1cfd446ec77de807f2a13c311451b6
KloudMax <25205995+kloudmax@users.noreply.github.com>
2022-03-23 15:44:43 +0900

    Update issue.sh

--- issue.sh
+++ issue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -z $1 ];
-	echo "Usage: $0 [domain name]"
-	exit
+if [ ! -z $1 ]; then
+    echo "Usage: $0 [domain name]"
+    exit
 fi
 
 ./acme.sh --issue --dns --force -d $1 -d *.$1 --yes-I-know-dns-manual-mode-enough-go-ahead-please
